ece0d1922a7d1cd1dc142ef0ffb01c0377d203c8
KevinRobichon <kevin.robichon@gmail.com>
2014-10-09 15:02:54 +0200

    moved conf in single file

--- init.el
+++ init.el
@@ -1,9 +1,21 @@
-(add-to-list 'load-path (expand-file-name "init" user-emacs-directory))
-
+(require 'cl)
 (require 'iso-transl)
-(require 'init-package)
-(package-declare 'evil                                    ;VIM
+
+(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("org" . "http://orgmode.org/elpa/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")))
+
+(defun package-declare (&rest packages)
+  (package-initialize)
+  (unless package-archive-contents (package-refresh-contents))
+  (dolist (package packages)
+    (unless (package-installed-p package) (package-install package))))
+
+(package-declare 'use-package                             ;package loading
+                 'evil                                    ;VIM
                  'evil-leader                             ;leader key
+                 'evil-nerd-commenter                     ;comments
                  'flycheck                                ;on-the-fly syntax
                  'company                                 ;completion
                  'magit                                   ;GIT integration
@@ -29,20 +41,95 @@
                  'cyberpunk-theme
                  'base16-theme
                  )
+(require 'use-package)
 
-;; base
-(require 'init-look)
-(require 'init-ido)
-(require 'init-edit)
+;; look
+(menu-bar-mode -1)
+(if (display-graphic-p)
+    (progn
+      (tool-bar-mode -1)
+      (scroll-bar-mode -1)))
 
-;; minor modes
-(require 'init-evil)
-(require 'init-flycheck)
-(require 'init-company)
+(setq inhibit-startup-message t)
+(setq inhibit-startup-echo-area-message t)
 
-;; major modes
-(require 'init-c)
-(require 'init-web)
-(require 'init-js)
+(load-theme 'grandshell t)
+
+(powerline-default-theme)
+
+(if (display-graphic-p)
+    (set-face-attribute 'default nil :height 110))
+
+;; ido-mode
+(ido-mode t)
+(ido-everywhere t)
+(ido-ubiquitous-mode t)
+(flx-ido-mode t)
+
+(setq ido-enable-flex-matching t)
+(setq ido-use-filename-at-point nil)
+(setq ido-use-virtual-buffers t)
+
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+;; edit
+(global-auto-revert-mode t)
+(show-paren-mode 1)
+
+(setq-default ring-bell-function 'ignore
+              tab-width 4
+              indent-tabs-mode nil
+              show-trailing-whitespace t
+              make-backup-files nil
+              auto-save-list-file-name nil
+              auto-save-default nil
+              )
+
+(dolist (hook '(shell-mode-hook
+                compilation-mode-hook
+                erc-mode-hook
+                ))
+  (add-hook hook
+            (lambda ()
+              (setq show-trailing-whitespace nil))))
+
+(use-package evil
+  :init
+  (progn
+    (use-package evil-leader
+      :init
+      (global-evil-leader-mode)
+      :config
+      (progn
+        (setq evil-leader/in-all-states t)
+        (evil-leader/set-leader ",")
+        (evil-leader/set-key
+          "b" 'ido-switch-buffer
+          "f" 'ido-find-file
+          "x" 'smex
+          "ci" 'evilnc-comment-or-uncomment-lines
+          )))
+    (setq evil-default-cursor t)
+    (evil-mode t)))
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+(add-hook 'after-init-hook #'global-company-mode)
+
+;; C
+(setq-default c-default-style "linux"
+              c-basic-offset 4)
+
+;; web
+(add-hook 'web-mode-hook
+          (lambda ()
+            (setq web-mode-markup-indent-offset 4)
+            (setq web-mode-css-indent-offset 4)
+            (setq web-mode-code-indent-offset 4)))
+
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 (provide 'init)
